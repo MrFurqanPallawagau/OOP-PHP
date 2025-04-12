7bea0ef970b84e9ce82e5c063f9c3d0b58717218
MrFurqanPallawagau <153912134+MrFurqanPallawagau@users.noreply.github.com>
2025-04-12 15:13:39 +0800

    Add files via upload

---
+++ ValidationUP.php
@@ -0,0 +1,33 @@
+<?php
+
+class ValidationUntil
+{
+  static function validate(LoginRequest $request)
+  {
+    if (!isset($request->username)){
+      throw new ValidationException("username is not set");
+    } else if (!isset($request->password)){
+      throw new ValidationException("Password is not set");
+    }else if (!isset($request->username)) {
+      throw new ValidationException("username is null");
+    } else if (!isset($request->password)) {
+    throw new ValidationException("Password is null");
+  }
+}
+
+
+
+
+static function validateReflection($request)
+{
+  $reflection = new ReflectionClass($request);
+  $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+  foreach ($properties as $property) {
+    if (!$property->isInitialized($request)) {
+      throw new ValidationException("$property->name is not set");
+} else if (is_null($property->getValue($request))) {
+  throw new ValidationException("$property->name is null");
+}
+}
+}
+}
